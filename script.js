5bfe8dd036a29a43d577df981c346836310711ec
Charlie-McDaddy <coding.0awnx@aleeas.com>
2021-11-23 09:55:09 +1000

    commit files to git

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//declare variables
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let direction = document.querySelector(".direction");
+let body = document.getElementById("gradient");
+
+function setGradient() {
+  body.style.background = `linear-gradient(${direction.value}, ${color1.value}, ${color2.value})`;
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+direction.addEventListener("input", setGradient);
